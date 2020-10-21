e5aa320e4970d5b7dae1817fefffef9618aed64d
Bryan <bryankoh137@hotmail.com>
2020-10-21 14:32:27 +0800

    removed react-component-library

--- react-component-library/.storybook/preview.js
+++
@@ -1,9 +0,0 @@
-import { configureActions } from "@storybook/addon-actions";
-
-// configure addon-actions
-configureActions({
-  depth: 100,
-  // Limit the number of  items logged into the actions panel
-  limit: 20,
-  clearOnStoryChange: true
-});
