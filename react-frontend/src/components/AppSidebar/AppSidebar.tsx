8170dad4d5aec93ef3d3061a5f84c27f1b87d1ee
Bryan <bryankoh137@hotmail.com>
2020-11-02 18:22:39 +0800

    react-frontend: Add sidebar partial

--- react-frontend/src/components/AppSidebar/AppSidebar.tsx
+++ react-frontend/src/components/AppSidebar/AppSidebar.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { EuiCollapsibleNav, EuiFlexItem, EuiHeaderSectionItemButton, EuiIcon } from '@elastic/eui';
 
 const AppSidebar = (): JSX.Element => {
-    return <div>Sidebar</div>;
+    const [navIsOpen, setIsNavOpen] = useState(false);
+    const [navIsDocked, setIsNavDocked] = useState(false);
+
+    return (
+        <EuiCollapsibleNav
+            id="collapsibleNavSideBar"
+            aria-label="Main navigation"
+            isOpen={navIsOpen}
+            isDocked={navIsDocked}
+            button={
+                <EuiHeaderSectionItemButton
+                    aria-label="Toggle main navigation"
+                    onClick={(): void => setIsNavOpen(!navIsOpen)}
+                >
+                    <EuiIcon type={'menu'} size="m" aria-hidden="true" />
+                </EuiHeaderSectionItemButton>
+            }
+            onClose={(): void => setIsNavOpen(false)}
+        >
+            <EuiFlexItem grow={false} style={{ flexShrink: 0 }}>
+                SOmerthing
+            </EuiFlexItem>
+        </EuiCollapsibleNav>
+    );
 };
 
 export default AppSidebar;
