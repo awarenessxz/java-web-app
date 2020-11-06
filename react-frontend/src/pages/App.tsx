aff8bff2cdb1ae690ad698696fa0fb93c5f463ee
Bryan <bryankoh137@hotmail.com>
2020-11-06 17:19:39 +0800

    react-frontend: added loading page partially

--- react-frontend/src/pages/App.tsx
+++ react-frontend/src/pages/App.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 import AppHeader from '../components/AppHeader/AppHeader';
 import AppContent from '../components/AppContent/AppContent';
 
 const App = (): JSX.Element => {
-    return (
-        <Router>
-            <AppHeader />
-            <Switch>
-                <Route exact path="/" component={AppContent} />
-                <Route exact path="/template" render={(props): JSX.Element => <AppContent {...props} position={2} />} />
-                <Route
-                    path="/template/:templateId"
-                    render={(props): JSX.Element => <AppContent {...props} position={3} />}
-                />
-            </Switch>
-        </Router>
-    );
+    const [isSiteReady, setSiteReadyState] = useState(false);
+
+    useEffect(() => {
+        // sleep for 5 seconds to show the loading component
+        setTimeout(() => {
+            setSiteReadyState(true);
+        }, 5000);
+    }, []);
+
+    if (isSiteReady) {
+        return (
+            <Router>
+                <AppHeader />
+                <Switch>
+                    <Route exact path="/" component={AppContent} />
+                    <Route
+                        exact
+                        path="/template"
+                        render={(props): JSX.Element => <AppContent {...props} position={2} />}
+                    />
+                    <Route
+                        path="/template/:templateId"
+                        render={(props): JSX.Element => <AppContent {...props} position={3} />}
+                    />
+                </Switch>
+            </Router>
+        );
+    }
+
+    return <div>Loading....</div>;
 };
 
 export default App;
